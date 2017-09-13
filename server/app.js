6a386e861eaf3505b78b3800ad70def803cf0f3b
mlvehr <mlvehr@163.com>
2017-09-13 10:03:31 +0800

    修改关于跨域配置

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.all("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", websiteCfg.ip);
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild");
   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
   if (req.method === "OPTIONS") {
