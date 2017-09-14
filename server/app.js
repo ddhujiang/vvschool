27b7322f63c9eae7e1acb45d58bbb5845e4c63aa
mlvehr <mlvehr@163.com>
2017-09-14 08:57:05 +0800

    修改文件上传接口返回路径

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.all("*", function (req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
@@ -36,7 +36,7 @@ app.all("*", function (req, res, next) {
   }
   else {next();}
 });
-// app.use('/', index);
+app.use('/static', express.static(path.join(__dirname, "public")));
 app.use("/users", users);
 app.use("/api", api);
 // app.use('/register',register);
